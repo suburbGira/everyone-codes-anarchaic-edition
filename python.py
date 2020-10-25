6955debb17bc49fa93ffbad345ce868f9add73c3
iFealarsi <64233341+iFealarsi@users.noreply.github.com>
2020-10-25 18:54:25 -0400

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,7 @@
 print("hi fealarsi")
 print("do whatever the fuck you want")
+YourMomGay = int(input("how much is 2+2 "))
+if YourMomGay == 4:
+    print('you are right')
+else:
+    print('you are not right')
